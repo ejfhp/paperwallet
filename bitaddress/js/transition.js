d7982e79409e1b8340a75e1818257a789f12cce2
Diego <ejfhp5@gmail.com>
2020-12-10 23:31:02 +0100

    WIP

---
+++ bitaddress/js/transition.js
@@ -0,0 +1,16 @@
+var tabnames = ["gallery", "paperarea"]
+
+function ShowTab(tabname) {
+    tabnames.forEach( t => {
+        let tab = document.getElementById(t);
+        if (t == tabname) {
+            tab.style.display="block"
+            if t== paperarea {
+                ninja.tab.
+            }
+        } else {
+            tab.style.display="none"
+
+        } 
+    })
+  }
