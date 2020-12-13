ab648f8739dd582069d607e8b22b1869f9ff124d
Diego <ejfhp5@gmail.com>
2020-12-13 23:50:36 +0100

    Playing with Flutter WIP

--- bitpaper/lib/main.dart
+++ bitpaper/lib/main.dart
@@ -1,122 +1,91 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(BitPaperApp());
+  Map<String, String> arts = {
+    'bitcoin': 'art_bitcoin.jpg',
+    'bitcoin dragon': 'art_bitcoinsv.jpg',
+    'christmas': 'art_christmas.jpg',
+    'intro it': 'art_intro_it.jpg',
+    'intro': 'art_intro.jpg'
+  };
+  var title = "BitPaper - Bitcoin on Paper";
+  var bitPaper = BitPaper(title: title, arts: arts, selected: "bitcoin", child: new MainPage());
+  var app = MaterialApp(
+    theme: ThemeData(
+      primarySwatch: Colors.amber,
+    ),
+    home: bitPaper,
+  );
+  runApp(app);
 }
 
-class BitPaperApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'BitPaper',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.amber,
-      ),
-      home: MyHomePage(title: 'BitPaper - Bitcoin Paperwallet'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class BitPaper extends InheritedWidget {
+  final Map<String, String> arts;
   final String title;
+  final String selected;
+
+  BitPaper({this.title, this.arts, this.selected, @required Widget child, Key key})
+      : super(key: key, child: child);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
+    return false;
   }
 
+  static of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<BitPaper>();
+  }
+}
+
+class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     BottomAppBar bottomBar = BottomAppBar(
       color: Colors.amber,
-      child: Text("Bottom"),
+      child: Text("Status"),
     );
-    AppBar topBar = AppBar(title: Text(widget.title));
-    ListTile templateImg = ListTile(leading: Text("Template"));
-    ListView list = ListView(children: [
-      templateImg,
-      Text("Drawer"),
-    ]);
-    Drawer drawer = Drawer(child: list);
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    String title = BitPaper.of(context).title;
+    AppBar topBar = AppBar(title: Text(title));
+
     return Scaffold(
       appBar: topBar,
       bottomNavigationBar: bottomBar,
-      drawer: drawer,
+      drawer: ArtsMenu(),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class ArtsMenu extends StatelessWidget {
+  ArtsMenu();
+
+  Widget build(BuildContext context) {
+    List<Widget> artsList = new List<Widget>();
+    DrawerHeader header = DrawerHeader(child: Text("Paper Arts"));
+    artsList.add(header);
+    var arts = BitPaper.of(context).arts;
+    arts.forEach((key, value) {
+      var t = Text(key);
+      var i = Image(
+          image: NetworkImage('https://paperwallet.ejfhp.com/img/' + value));
+      ListTile tI = ListTile(
+        leading: i,
+        title: t,
+      );
+      artsList.add(tI);
+    });
+    ListView list = ListView(children: artsList);
+    return Drawer(
+      child: list,
     );
   }
 }
