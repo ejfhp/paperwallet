8d5ae3385c25cd55f46b485e1cc810e60e68a7c8
ejfhp <ejfhp5@gmail.com>
2020-12-15 20:03:18 +0100

    WIP

--- bitpaper/lib/main.dart
+++ bitpaper/lib/main.dart
@@ -1,15 +1,9 @@
 import 'package:flutter/material.dart';
 
+const appTitle = "BitPaper - Bitcoin on Paper";
+
 void main() {
-  Map<String, String> arts = {
-    'bitcoin': 'art_bitcoin.jpg',
-    'bitcoin dragon': 'art_bitcoinsv.jpg',
-    'christmas': 'art_christmas.jpg',
-    'intro it': 'art_intro_it.jpg',
-    'intro': 'art_intro.jpg'
-  };
-  var title = "BitPaper - Bitcoin on Paper";
-  var bitPaper = BitPaper(title: title, arts: arts, selected: "bitcoin", child: new MainPage());
+  var bitPaper = BitPaper();
   var app = MaterialApp(
     theme: ThemeData(
       primarySwatch: Colors.amber,
@@ -19,35 +13,30 @@ void main() {
   runApp(app);
 }
 
-class BitPaper extends InheritedWidget {
-  final Map<String, String> arts;
-  final String title;
-  final String selected;
-
-  BitPaper({this.title, this.arts, this.selected, @required Widget child, Key key})
-      : super(key: key, child: child);
-
+class BitPaper extends StatefulWidget {
   @override
-  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
-    return false;
-  }
-
-  static of(BuildContext context) {
-    return context.dependOnInheritedWidgetOfExactType<BitPaper>();
-  }
+  _BitPaperState createState() => _BitPaperState();
 }
 
-class MainPage extends StatelessWidget {
+class _BitPaperState extends State<BitPaper> {
+  Map<String, String> arts = {
+    'bitcoin': 'art_bitcoin.jpg',
+    'bitcoin dragon': 'art_bitcoinsv.jpg',
+    'christmas': 'art_christmas.jpg',
+    'intro it': 'art_intro_it.jpg',
+    'intro': 'art_intro.jpg'
+  };
+  String selected = "bitcoin";
+
   @override
   Widget build(BuildContext context) {
     BottomAppBar bottomBar = BottomAppBar(
       color: Colors.amber,
       child: Text("Status"),
     );
-    String title = BitPaper.of(context).title;
-    AppBar topBar = AppBar(title: Text(title));
+    AppBar topBar = AppBar(title: Text(appTitle));
 
-    return Scaffold(
+    Scaffold scaff =  Scaffold(
       appBar: topBar,
       bottomNavigationBar: bottomBar,
       drawer: ArtsMenu(),
@@ -62,6 +51,28 @@ class MainPage extends StatelessWidget {
         ),
       ),
     );
+    return MainPage(child: scaff, state: this);
+  }
+
+  void setSelected(String sel) {
+    setState(() {
+      selected = sel;
+    });
+  }
+}
+
+
+class Menu extends InheritedWidget {
+  final _BitPaperState state;
+  Menu({Key key, Widget child, this.state}): super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(covariant Menu oldWidget) {
+    return oldWidget.state.selected != state.selected;
+  }
+
+  static Menu of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<Menu>();
   }
 }
 
@@ -69,23 +80,61 @@ class ArtsMenu extends StatelessWidget {
   ArtsMenu();
 
   Widget build(BuildContext context) {
+    _BitPaperState appState = MainPage.of(context).state;
     List<Widget> artsList = new List<Widget>();
     DrawerHeader header = DrawerHeader(child: Text("Paper Arts"));
     artsList.add(header);
-    var arts = BitPaper.of(context).arts;
+    var arts = appState.arts;
+    var selected = appState.selected;
     arts.forEach((key, value) {
-      var t = Text(key);
+      Widget t;
+      if (key == selected) {
+        t = Text(key, style: TextStyle(fontWeight: FontWeight.bold));
+      } else {
+        t = Text(key);
+      }
       var i = Image(
           image: NetworkImage('https://paperwallet.ejfhp.com/img/' + value));
       ListTile tI = ListTile(
         leading: i,
         title: t,
+        onTap: () {appState.setSelected(key);},
       );
       artsList.add(tI);
     });
     ListView list = ListView(children: artsList);
     return Drawer(
       child: list,
+    );
+  }
+}
+
+class Menu extends InheritedWidget {
+  final _BitPaperState state;
+  Menu({Key key, Widget child, this.state}): super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(covariant Menu oldWidget) {
+    return oldWidget.state.selected != state.selected;
+  }
+
+  static Menu of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<Menu>();
+  }
+}
+
+class ArtsWallet  extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    _BitPaperState appState = MainPage.of(context).state;
+    var selectedImage = appState.arts[appState.selected];
+    return Container(
+     child: Center(
+       child: Image(
+          image: NetworkImage('https://paperwallet.ejfhp.com/img/' + selectedImage)
+     ), 
+     ),
     );
   }
 }
